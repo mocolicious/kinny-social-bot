f3f5488c5d7e8e197c0b9944c518e9e82ce916bc
chancity <hawbakerchancey@gmail.com>
2019-04-10 11:25:11 -0700

    Add project files.

---
+++ kinny-social-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kinny_social_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
